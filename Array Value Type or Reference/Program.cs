76c1130183120eb42d71e7da0059c340adecb469
Kulpot <kulpotwahu@gmail.com>
2023-07-31 18:28:18 +0800

    Add project files.

---
+++ Array Value Type or Reference/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=aQ8_RKDGMtw&list=PLRwVmtr-pp07QlmssL4igw1rnrttJXerL&index=8
+//
+
+//class MeClass   // reference type
+struct MeClass // value type
+{
+    public int MeField;
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        int[] meInts = new int[] { 1, 5, 4, 7 };    //value type
+
+        //Console.WriteLine(meInts[2]);
+
+        MeClass[] meClasses = new MeClass[3];   // reference type
+        meClasses[1] = new MeClass(); // instanciate new object 
+        //meClasses[1].MeField = 5;
+        Console.WriteLine(meClasses[1].MeField);
+    }
+}
